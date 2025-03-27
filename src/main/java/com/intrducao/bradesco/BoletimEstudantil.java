4843d008599144966415bb481fa73a74371a033f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-03-27 10:02:17 -0300

    Anatomia das classes 4

---
+++ src/main/java/com/intrducao/bradesco/BoletimEstudantil.java
@@ -0,0 +1,26 @@
+package com.intrducao.bradesco;
+
+import java.util.Scanner;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int mediaFinal = scn.nextInt();
+
+        if(mediaFinal < 6) {
+
+            System.out.println("REPROVADO");
+        } else if(mediaFinal == 6) {
+
+            System.out.println("PROVA MINERVA");
+        } else {
+
+            System.out.println("APROVADO");
+        }
+
+        scn.close();
+    }
+}
