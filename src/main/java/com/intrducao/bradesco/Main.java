3d69ad3f6054b1f9ae56285b5c36858c69830876
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-03-27 09:16:08 -0300

    teste com if composto

--- src/main/java/com/intrducao/bradesco/Main.java
+++ src/main/java/com/intrducao/bradesco/Main.java
@@ -1,7 +1,25 @@
 package com.intrducao.bradesco;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scn = new Scanner(System.in);
+
+        int idade = scn.nextInt();
+
+        if(idade < 18) {
+
+            System.out.println("Menor de idade");
+        } else if(idade > 18 && idade < 64) {
+
+            System.out.println("Maior de Idade");
+        } else {
+
+            System.out.println("Idoso");
+        }
+
+        sc.close();
     }
 }
