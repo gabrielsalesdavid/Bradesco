cf79116ac07a328d007dfb04d4a18517a9a5027d
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-03-28 19:02:36 -0300

    operator aritm

---
+++ src/main/java/com/operador/bradesco/Operador.java
@@ -0,0 +1,13 @@
+package com.operador.bradesco;
+
+public class Operador {
+
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+    }
+}
