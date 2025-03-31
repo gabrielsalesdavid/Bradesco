428f8808f537bf53651186a0deed12d9029419d5
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-03-30 22:29:05 -0300

    methods with smartTv

---
+++ src/main/java/com/methods/Aula01.java
@@ -0,0 +1,28 @@
+package com.methods;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+
+        SmarTv smt = new SmarTv();
+        smt.diminuirVolume();
+        smt.diminuirVolume();
+        smt.diminuirVolume();
+        smt.aumentarVolume();
+
+        System.out.println("Canal atual: " + smt.canal);
+        smt.mudarCanal(13);
+        System.out.println("Canal atual: " + smt.canal);
+        System.out.println("Volume atual: " + smt.volume);
+
+        System.out.println("TV ligada? " + smt.ligada);
+        System.out.println("Canal atual: " + smt.canal);
+        System.out.println("Volume atual: " + smt.volume);
+
+        smt.ligar();
+        System.out.println("TV ligada? " + smt.ligada);
+
+        smt.desligar();
+        System.out.println("TV ligada? " + smt.ligada);
+    }
+}
